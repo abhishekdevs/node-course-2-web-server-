e472f2f3127d4a4afbeee16b2a3887d93fd5ff9a
abhishek <abhi.abhishek.akumar@gmail.com>
2018-06-27 15:07:59 +0530

    updating heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const hbs     = require('hbs');
+const fs      = require('fs');
+
+const port = process.env.PORT || 3000
 
 var app = express();
 app.set('view engine', 'hbs');
@@ -12,7 +15,7 @@ app.use((req, res, next)=>{
   var log = `${now} : ${req.method} ${req.url}`
   console.log(log);
   fs.appendFile('server.log',log + '\n');
-  next(); 
+  next();
 });
 
 hbs.registerHelper('getCurrentYear',() =>{
@@ -43,6 +46,6 @@ res.send({
 });
 });
 
-app.listen(3000, ()=>{
-  console.log('Server is up on port 3000');
+app.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
 });
