52660ede30f069c2cbb4a13ecdb704e72ad7a98d
abhishek <abhi.abhishek.akumar@gmail.com>
2018-06-27 15:19:55 +0530

    portfolio page added

--- server.js
+++ server.js
@@ -39,6 +39,14 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio.hbs',{
+    pageTitle:'Portfolio Page'
+
+  });
+});
+
+
 app.get('/bad',(req, res) =>{
 
 res.send({
