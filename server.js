ad15accd2227e19c90567c037e545b700cd359b6
abhishek <abhi.abhishek.akumar@gmail.com>
2018-06-27 13:27:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs     = require('hbs');
+
+var app = express();
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log + '\n');
+  next(); 
+});
+
+hbs.registerHelper('getCurrentYear',() =>{
+  //return 'test';
+  return new Date().getFullYear()
+});
+
+app.get('/',(req, res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage:'Welcome to ABC Corporation'
+  });
+
+
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle:'About Page'
+
+  });
+});
+
+app.get('/bad',(req, res) =>{
+
+res.send({
+    errorMessage :'Invalid Request'
+});
+});
+
+app.listen(3000, ()=>{
+  console.log('Server is up on port 3000');
+});
